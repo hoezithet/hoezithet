c85b491ebe3d310a357a12dc21237cb84bf9fde9
Floris De Feyter <floris@hoezithet.nu>
2021-09-03 19:20:30 +0200

    Add emphasize arg to ruler

--- src/components/shortcodes/ruler.tsx
+++ src/components/shortcodes/ruler.tsx
@@ -5,11 +5,12 @@ import { getColor } from "../../colors";
 import { DrawingContext } from "./drawing";
 import { useStyles } from "./line";
 
-const Ruler = ({x=0, y=0, angle=0, size=30, major=10, unit="m", color="yellow", tickColor="dark_gray", opacity=1, lineWidth=1}) => {
+const Ruler = ({x=0, y=0, angle=0, size=30, major=10, emphasize=null, unit="m", color="yellow", tickColor="dark_gray", opacity=1, lineWidth=1}) => {
     const {xScale, yScale} = useContext(DrawingContext);
     color = getColor(color);
     tickColor = getColor(tickColor);
-    const classes = useStyles({color: tickColor, lineWidth: lineWidth, opacity: opacity});
+    const tickClasses = useStyles({color: tickColor, lineWidth: lineWidth, opacity: opacity});
+    const emphasizeTickClasses = useStyles({color: tickColor, lineWidth: lineWidth*2, opacity: opacity});
     const tickMargin = 20;
     const majorTickLength = 10;
     const minorTickLength = 5;
@@ -25,8 +26,10 @@ const Ruler = ({x=0, y=0, angle=0, size=30, major=10, unit="m", color="yellow", 
     const scaledSize = Math.sqrt(Math.pow(xScaledSize, 2) + Math.pow(yScaledSize, 2))
     const width = scaledSize + 2*tickMargin;
 
+    const rulerId = _.uniqueId("rulerRect-");
+
     const rulerRect = (
-        <rect x={-tickMargin} y="0" width={width} height={rulerHeight} id="rulerRect" rx={rulerRx} fill={color}/>
+        <rect x={-tickMargin} y="0" width={width} height={rulerHeight} id={rulerId} rx={rulerRx} fill={color}/>
     );
     return (
         <>
@@ -34,24 +37,32 @@ const Ruler = ({x=0, y=0, angle=0, size=30, major=10, unit="m", color="yellow", 
             <clipPath id="rulerClip">
                 { rulerRect }
             </clipPath>
-            <use xlinkHref="#rulerRect" />
+            <use xlinkHref={`#${rulerId}`}  />
             <g clipPath="url(#rulerClip)">
             {
                 _.range(0, size + 1).map(i => {
                     const xPos = scaledSize*i/size;
                     const isMajor = major && i % major === 0;
                     const isSemiMajor = major && i*2 % major === 0;
+                    const isEmphasize = i === emphasize;
                     return (
                     <g key={i}>
-                        <path d={`M ${xPos},0 v ${isMajor || isSemiMajor ? majorTickLength : minorTickLength}`} className={classes.line}/>
+                        <path d={`M ${xPos},0 v ${isMajor || isSemiMajor ? majorTickLength : minorTickLength}`}
+                              className={isEmphasize ? emphasizeTickClasses.line : tickClasses.line} />
                         { isMajor ?
-                          <text x={xPos} y={majorTickLength} fontSize="smaller" alignmentBaseline="baseline" dominantBaseline="hanging" textAnchor="middle">{ i }</text>
+                          <text x={xPos} y={majorTickLength} fontSize="x-small"
+                                fill={tickColor}
+                                alignmentBaseline="baseline" dominantBaseline="hanging"
+                                textAnchor="middle" fontWeight={isEmphasize ? "bold" : "normal"}>
+                              { i }
+                          </text>
                           : null }
                     </g>
                     )
                 })
             }
-            <text x={0} y={rulerHeight - minorTickLength} fontSize="x-small">{ unit }</text>
+            <text x={0} y={rulerHeight - minorTickLength} fontSize="x-small"
+                fill={tickColor}>{ unit }</text>
             </g>
         </g>
         </>
