70e2c059a9729c4e24fc77dee8dec2305fcdebb4
Floris De Feyter <floris@hoezithet.nu>
2021-10-25 08:39:28 +0200

    Add AnnotArrow

--- src/components/shortcodes/annot.tsx
+++ src/components/shortcodes/annot.tsx
@@ -4,6 +4,31 @@ import { theme } from "../theme";
 import { DrawingContext } from "./drawing";
 import { ArrowLine } from "./arrow";
 import { SvgNote } from "./svgNote";
+import { useStyles } from "./line";
+import _ from "lodash";
+
+
+type Coordinate = {
+    x: number,
+    y: number,
+}
+
+type AnnotProps = {
+    annot: string|Coordinate,
+    target: string|Coordinate|Coordinate[],
+    margin?: number,
+    anchorRadiusTarget?: number|null,
+    anchorRadiusAnnot?: number|null,
+    hAlignAnnot?: string,
+    vAlignAnnot?: string,
+    hAlignTarget?: string,
+    vAlignTarget?: string,
+    color?: string,
+    opacity?: number,
+    lineWidth?: number,
+    hideHead?: boolean,
+    dashed?: boolean
+}
 
 
 const calcAnchorAngleAnnot = (dx, dy) => {
@@ -15,6 +40,7 @@ const calcAnchorAngleAnnot = (dx, dy) => {
         return dy >= 0 ? -90 : 90;
     }
 };
+
 
 const calcAnchorAngleTarget = (dx, dy) => {
     if (dx >= 0 && dy >=0) {
@@ -32,6 +58,7 @@ const calcAnchorAngleTarget = (dx, dy) => {
     }
 };
 
+
 const canonicalAngle = (angle) => {
     // Return equivalent angle between ]-180; 180]
     const posAngle = ((angle % 360) + 360) % 360;
@@ -41,54 +68,100 @@ const canonicalAngle = (angle) => {
     )
 }
 
-export const Annot = ({
-    xAnnot, yAnnot, xTarget, yTarget, margin=theme.spacing(1),
-    anchorAngleTarget=null, anchorRadiusTarget=20,
-    anchorAngleAnnot=null, anchorRadiusAnnot=20,
-    lineColor="light_gray", lineOpacity=1, lineWidth=2,
-    showArrow=true, showLine=true, dashed=false, showBackground=false,
-    backgroundColor="light_gray", backgroundOpacity=0.5, children
-}) => {
+
+const getAngleFromAlign = (hAlign, vAlign) => {
+    if (vAlign === "center" && hAlign === "right") {
+        return 0;
+    } else if (vAlign === "top" && hAlign === "right") {
+        return 45;
+    } else if (vAlign === "top" && hAlign === "center") {
+        return 90;
+    } else if (vAlign === "top" && hAlign === "left") {
+        return 135;
+    } else if (vAlign === "center" && hAlign === "left") {
+        return 180;
+    } else if (vAlign === "bottom" && hAlign === "left") {
+        return -135;
+    } else if (vAlign === "bottom" && hAlign === "center") {
+        return -90;
+    } else if (vAlign === "bottom" && hAlign === "right") {
+        return -45;
+    }
+}
+
+
+const getElCoord = (el: Element, hAlign: string, vAlign: string) => {
+    const rect = el.getBoundingClientRect()
+    const originRect = el.closest("svg")?.getBoundingClientRect();
+    const [tLeft, tTop, tRight, tBottom] = [
+        rect?.left - (originRect?.x || 0),
+        rect?.top - (originRect?.y || 0),
+        rect?.right - (originRect?.x || 0),
+        rect?.bottom - (originRect?.y || 0),
+    ];
+    const [tCenterX, tCenterY] = [
+        tLeft + rect?.width/2,
+        tTop + rect?.height/2,
+    ];
+    return {
+        x: hAlign === "left" ? tLeft : (hAlign === "right" ? tRight : tCenterX),
+        y: vAlign === "top" ? tTop : (vAlign === "bottom" ? tBottom : tCenterY),
+    }
+}
+
+
+const convertToCoord = (annotOrTarget: string|Coordinate, hAlign: string, vAlign: string, xScale, yScale) => {
+    if (typeof document === "undefined") {
+        return {x: 0, y: 0};
+    }
+    if (typeof annotOrTarget === "string") {
+        const el = document.querySelector(annotOrTarget);
+        if (el !== null) {
+            return getElCoord(el, hAlign, vAlign);
+        } else {
+            return {x: 0, y: 0};
+        }
+    } else if (xScale !== null && yScale !== null) {
+        return {x: xScale(annotOrTarget.x), y: yScale(annotOrTarget.y)};
+    } else {
+        return annotOrTarget;
+    }
+}
+
+
+export const AnnotArrow = ({
+    annot, target,
+    margin=theme.spacing(1),
+    anchorRadiusTarget=20, anchorRadiusAnnot=20,
+    hAlignAnnot="center", vAlignAnnot="top",
+    hAlignTarget="center", vAlignTarget="top",
+    color="light_gray", opacity=1, lineWidth=2,
+    hideHead=false, dashed=false,
+}: AnnotProps) => {
     const {xScale, yScale} = useContext(DrawingContext);
 
-    if (xScale && yScale) {
-        [xTarget, xAnnot] = [xTarget, xAnnot].map(xScale);
-        [yTarget, yAnnot] = [yTarget, yAnnot].map(yScale);
-    }
+    const annotCoord = convertToCoord(annot, hAlignAnnot, vAlignAnnot, xScale, yScale);
+    target = Array.isArray(target) ? target : [target];
+    const targetCoords = target.map(t => convertToCoord(t, hAlignTarget, vAlignTarget, xScale, yScale))
 
-    const [dx, dy] = [xTarget - xAnnot, yTarget - yAnnot];
-    anchorAngleTarget = canonicalAngle(anchorAngleTarget === null ? calcAnchorAngleTarget(dx, dy) : anchorAngleTarget);
-    anchorAngleAnnot = canonicalAngle(anchorAngleAnnot === null ? calcAnchorAngleAnnot(dx, dy) : anchorAngleAnnot);
+    const anchorAngleAnnot = getAngleFromAlign(hAlignAnnot, vAlignAnnot);
+    const anchorAngleTarget = getAngleFromAlign(hAlignTarget, vAlignTarget);
 
-    const arrowLine = <ArrowLine xStart={xAnnot} yStart={yAnnot} xEnd={xTarget} yEnd={yTarget}
-            margin={margin}
-            anchorAngleStart={anchorAngleAnnot} anchorRadiusStart={anchorRadiusAnnot}
-            anchorAngleEnd={anchorAngleTarget} anchorRadiusEnd={anchorRadiusTarget}
-            lineColor={lineColor} lineWidth={lineWidth} dashed={dashed} showArrow={showArrow}
-            opacity={lineOpacity} useContextScale={false} />;
-
-    let hAlign, vAlign;
-    if (anchorAngleAnnot >= -45 && anchorAngleAnnot < 45) {
-        [hAlign, vAlign] = ["right", "center"];
-    } else if (anchorAngleAnnot >= 45 && anchorAngleAnnot < 135) {
-        [hAlign, vAlign] = ["center", "top"];
-    } else if (anchorAngleAnnot >= -135 && anchorAngleAnnot < -45) {
-        [hAlign, vAlign] = ["center", "bottom"];
-    } else if (anchorAngleAnnot >= 135 || anchorAngleAnnot < -135) {
-        [hAlign, vAlign] = ["left", "center"];
-    }
+    const arrowLines = targetCoords.filter(t => t !== undefined).map((targetCoord, i) => (
+        <g key={i}>
+            <ArrowLine xStart={annotCoord.x} yStart={annotCoord.y} xEnd={targetCoord.x} yEnd={targetCoord.y}
+                margin={margin}
+                anchorAngleStart={anchorAngleAnnot} anchorRadiusStart={anchorRadiusAnnot}
+                anchorAngleEnd={anchorAngleTarget} anchorRadiusEnd={anchorRadiusTarget}
+                color={color} lineWidth={lineWidth} dashed={dashed} showArrow={!hideHead}
+                opacity={opacity} useContextScale={false} />
+        </g>
+    ));
 
     return (
         <>
-        <SvgNote x={xAnnot} y={yAnnot} showBackground={showBackground} backgroundColor={showBackground ? backgroundColor : "none"}
-        backgroundOpacity={backgroundOpacity} hAlign={hAlign}
-        vAlign={vAlign} useContextScale={false}>
-            { children }
-        </SvgNote>
         {
-            showLine ?
-            arrowLine
-            : null
+            arrowLines
         }
         </>
     );
