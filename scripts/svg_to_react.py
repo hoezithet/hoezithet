c0ae03ae6524de7bdf6f94f02811f2bfcdf69ab7
Floris De Feyter <floris@hoezithet.nu>
2021-11-06 10:44:47 +0100

    Add: include defs for pattern fills

--- scripts/svg_to_react.py
+++ scripts/svg_to_react.py
@@ -2,7 +2,10 @@
 
 import argparse
 from pathlib import Path
+import re
 import sys
+import textwrap
+
 sys.path.append('/usr/share/inkscape/extensions')
 
 from inkex.elements import load_svg
@@ -39,14 +42,6 @@ def kebab_to_camel(kebab):
     return ''.join([w.title() if i > 0 else w
                     for i, w in enumerate(kebab.split('-'))])
 
-def style_str_to_attrs(node):
-    for el in node.xpath('*[@style]'):
-        for style in el.findone('@style').split(';'):
-            k, v = style.split(':')
-            el.set(k, v)
-        el.pop('style')
-    return node
-
 def drop_id(node):
     node.pop('id')
 
@@ -55,72 +50,58 @@ def drop_ids(node):
         el.pop('id')
     return node
 
+def get_linked_nodes(node):
+    if node.href is not None:
+        return [node, *[n for n in get_linked_nodes(node.href)]]
+    else:
+        return [node]
+
+def get_fill_defs(node):
+    fill = node.style['fill']
+    if fill is None or len(fill) < 3 or fill[:3] != 'url':
+        return []
+    else:
+        linked_node = node.root.getElementById(fill)
+        return get_linked_nodes(linked_node)
+
 ungroup = UngroupDeep()
 ungroup.parse_arguments(args=[])
-
-norm_width = 100
-norm_height = 100
 
 for group in groups:
     g_id = group.get_id()
     bbox = group.bounding_box()
-    children = list(group)
+    norm_width = bbox.width
+    norm_height = bbox.height
 
-    group.set('transform', f'scale({norm_width/bbox.width} {norm_height/bbox.height}) translate({-bbox.left} {-bbox.top})')
-    ungroup._deep_ungroup(group)
-
-    new_children = []
-
-    for child in children:
-        new_child = child.to_path_element()
-        new_child.apply_transform()
-        new_children.append(new_child)
- 
-    children_str = '\n'.join([
-        c.tostring().decode('utf-8')
-        for c in new_children
+    group_str = group.tostring().decode('utf-8')
+    class_name = g_id.title().replace('_', '').replace('-', '')
+    
+    defs = [d
+        for node in group.xpath('//*[contains(@style,"fill:url(#")]')
+        for d in get_fill_defs(node)
+    ]
+    defs_str = ''.join([
+        d.tostring().decode('utf-8')
+        for d in defs
     ])
+    defs_str = textwrap.indent(f'{4*" "}<defs>\n{4*" "}{textwrap.indent(defs_str, 4*" ")}</defs>', 8*" ") if len(defs) > 0 else ""
 
-    class_name = g_id.title()
-    props_name = f'{class_name}Props'
+    file_text = f'''import React from "react";
+import withSizePositionAngle from "components/withSizePositionAngle";
 
-    file_text = f'''
-import React from "react";
 
-type {props_name} = {{
-    width: number|null,
-    height: number|null,
-    x: number,
-    y: number
-}}
-
-const {class_name} = ({{width=null, height=null, x, y}}: {props_name}) => {{
-    let xScale, yScale;
-
-    if (width === null && height !== null) {{
-	yScale = height / {norm_height};
-        xScale = yScale
-    }} else if (height === null && width !== null) {{
-	xScale = width / {norm_width};
-        yScale = xScale
-    }} else if (width !== null && height !== null) {{
-	xScale = width / {norm_width};
-	yScale = height / {norm_height};
-    }} else {{
-        xScale = 1;
-        yScale = 1;
-    }}
-
-    const xShift = props.x;
-    const yShift = props.y;
-
+const _{class_name} = () => {{
     return (
-        <g transform={{`scale(${{xScale}} ${{yScale}}) translate(${{xShift}} ${{yShift}})`}}>
-            {children_str}
-        </g>
+        <g transform="translate({-bbox.left} {-bbox.top})">
+{defs_str}
+{textwrap.indent(group_str, 12*" ")}{4*" "}</g>
     );
 }}
 
-export default {class_name};
-'''
-    print(file_text)
+const {class_name} = withSizePositionAngle(_{class_name}, {norm_width}, {norm_height}, true);
+
+export default {class_name};'''
+
+    if not dest.exists():
+        dest.mkdir(parents=True)
+    (dest / f'{g_id}.tsx').write_text(file_text)
