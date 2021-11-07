17db310babf979b737f7742cc31ae216c9271af6
Floris De Feyter <floris@hoezithet.nu>
2021-11-07 11:35:28 +0100

    Refactor DrawingGrid

--- src/components/shortcodes/drawingGrid.tsx
+++ src/components/shortcodes/drawingGrid.tsx
@@ -3,51 +3,83 @@ import _ from "lodash";
 
 import { getColor } from "../../colors";
 import { DrawingContext } from "./drawing";
-import { useStyles } from "./line";
 
-const DrawingGrid = ({color="blue", major=10, opacity=0.1, lineWidth=1}) => {
+
+type GridLineProps = {
+    i: number,
+    scale: (number) => number,
+    length: number,
+    direction: 'h' | 'v',
+    showText: boolean,
+    color: string,
+    opacity: number,
+    lineWidth: number,
+};
+
+const GridLine = ({
+    i, scale, length, direction, showText=false,
+    color, opacity, lineWidth
+}: GridLineProps) => {
+    return (
+        <>
+          <path d={`M ${direction === 'v' ? scale(i) : 0},${direction === 'h' ? scale(i) : 0} ${direction} ${length}`} stroke={color} strokeWidth={lineWidth} strokeOpacity={opacity} />
+          { showText ?
+              <text x={direction === 'v' ? scale(i) : 0} y={direction === 'h' ? scale(i) : length} fill={color}>{ i }</text>
+              : null
+          }
+        </>
+    );
+};
+
+type GridLinesProps = {
+    start: number,
+    end: number,
+    step: number,
+    scale: (number) => number,
+    length: number,
+    direction: 'h' | 'v',
+    showText: boolean,
+    color: string,
+    opacity: number,
+    lineWidth: number,
+};
+
+
+const GridLines = ({
+    start, end, step, scale, length, direction,
+    showText=false, color, opacity, lineWidth
+}: GridLinesProps) => {
+    return (
+        <>
+        {
+            _.range(Math.floor(start), Math.ceil(end) + 1, step*(start < end ? 1 : -1)).map(i =>
+                <g key={i}>
+                  <GridLine i={i} scale={scale} length={length} direction={direction}
+                     color={color} lineWidth={lineWidth} opacity={opacity} showText={showText} />
+                </g>
+            )
+        }
+        </>
+    );
+};
+
+const DrawingGrid = ({color="blue", major=10, minor=1, opacity=0.1, lineWidth=1}) => {
     const {xScale, yScale, width, height} = useContext(DrawingContext);
     color = getColor(color);
-    const minorClasses = useStyles({color: color, lineWidth: lineWidth, opacity: opacity});
-    const majorClasses = useStyles({color: color, lineWidth: 2*lineWidth, opacity: opacity});
 
     const [xMin, xMax] = xScale.domain();
     const [yMin, yMax] = yScale.domain();
 
     return (
         <>
-        {
-            _.range(Math.floor(xMin), Math.ceil(xMax) + 1).map(i => {
-                const isMajor = major && i % major == 0;
-
-                return (
-                    <g key={i}>
-                    <path d={`M ${xScale(i)},0 v ${height}`} className={isMajor ? majorClasses.line : minorClasses.line}/>
-                    { isMajor ?
-                        <text x={xScale(i)} y={height}>{ i }</text>
-                        : null
-                    }
-                    </g>
-                );
-            }
-            )
-        }
-        {
-            _.range(Math.floor(yMin), Math.ceil(yMax) + 1).map(i => {
-                const isMajor = major && i % major == 0;
-
-                return (
-                    <g key={i}>
-                    <path d={`M 0,${yScale(i)} h ${width}`} className={isMajor ? majorClasses.line : minorClasses.line}/>
-                    { isMajor ?
-                        <text x="0" y={yScale(i)}>{ i }</text>
-                        : null
-                    }
-                    </g>
-                );
-            }
-            )
-        }
+          <GridLines start={xMin} end={xMax} step={minor} scale={xScale} length={height} direction='v'
+            color={color} lineWidth={lineWidth} opacity={opacity} /> 
+          <GridLines start={xMin} end={xMax} step={major} scale={xScale} length={height} direction='v'
+            color={color} lineWidth={2*lineWidth} opacity={opacity} showText />
+          <GridLines start={yMin} end={yMax} step={minor} scale={yScale} length={width} direction='h'
+            color={color} lineWidth={lineWidth} opacity={opacity} /> 
+          <GridLines start={yMin} end={yMax} step={major} scale={yScale} length={width} direction='h'
+            color={color} lineWidth={2*lineWidth} opacity={opacity} showText />
         </>
     );
 };
