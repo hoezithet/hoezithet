fa9c08362d48ccef516fe4798f5c7777ae0dd0fd
Floris De Feyter <floris@hoezithet.nu>
2021-11-15 10:39:39 +0100

    Refac: pass array of joint keypoints to AnimatedHoseCycle

--- content/fysica/grootheden_eenheden/eenheden_omzetten/drawings/walk_cycle.tsx
+++ content/fysica/grootheden_eenheden/eenheden_omzetten/drawings/walk_cycle.tsx
@@ -12,72 +12,179 @@ const WalkCycleContext = React.createContext({tl: null});
 
 const _WalkCycle = () => {
     const [tl, setTl] = React.useState(() => gsap.timeline({repeat: -1}));
-    const legWidth = 10;
-    const legLength = 60;
-    const bendRadius = 2.0;
+    const bodyRef = React.useRef();
+
+    const legWidth = 8;
+    const legLength = 40;
+    const legBendRadius = 2.0;
+    const bodyWidth = 15;
+    const bodyHeight = 32;
+    const bodyBendRadius = -10.0;
+    const armSwingAngle = Math.PI/6;
+    const armLength = bodyHeight;
+    const armWidth = legWidth*3/4;
+    const armBendRadius = -2.0;
+    const headSize = 14;
+
     const color = "#000000";
-    const footAmplX = 15;
-    const footAmplY = 15;
-    const hipAmpl = 2;
-    const freq = 0.75;
-    const hip = {
-        x: 30,
-        y: 30
+    const footAmpl = {
+        x: 12,
+        y: 8
     };
+    const hipAmpl = {
+        x: 0,
+        y: 1
+    };
+    const shoulderAmpl = {
+        x: 0,
+        y: 0,
+    };
+    const handAmpl = {
+        x: 10,
+        y: 3,
+    };
+
+    const freq = 0.75;
+    const period = 1/freq;
+    const numKeypoints = Math.round(24/freq);
+    const kps = range(0, 2*period, period/numKeypoints);
+
+    const foot = {
+        x: 50,
+        y: 100 - legWidth/2
+    };
+    const hip = {
+        x: foot.x,
+        y: foot.y - legLength * 0.8
+    };
+    const shoulder = {
+        x: hip.x + bodyHeight * 0.1,
+        y: hip.y - bodyHeight * 0.65
+    };
+    const hand = {
+        x: shoulder.x,
+        y: shoulder.y + armLength * 0.8
+    };
+    const head = {
+        x: hip.x + headSize*0.6,
+        y: shoulder.y - headSize
+    }
+
+    const hipKps = getKeypoints(kps, t => getWalkingHip(t, hip, hipAmpl, freq, 0));
+
+    const lFootKps = getKeypoints(kps, t => getWalkingFoot(t, foot, footAmpl, freq, 0));
+    const lShoulderKps = getKeypoints(kps, t => getWalkingShoulder(t, shoulder, shoulderAmpl, freq, 0));
+    const lHandKps = getKeypoints(kps, t => getWalkingHand(t, hand, handAmpl, freq, Math.PI));
+    const lLegKps = getHoseKeypoints(hipKps, lFootKps);
+    const lArmKps = getHoseKeypoints(lShoulderKps, lHandKps);
+
+    const rFootKps = getKeypoints(kps, t => getWalkingFoot(t, foot, footAmpl, freq, Math.PI));
+    const rShoulderKps = getKeypoints(kps, t => getWalkingShoulder(t, shoulder, shoulderAmpl, freq, Math.PI));
+    const rHandKps = getKeypoints(kps, t => getWalkingHand(t, hand, handAmpl, freq, 0));
+    const rLegKps = getHoseKeypoints(hipKps, rFootKps);
+    const rArmKps = getHoseKeypoints(rShoulderKps, rHandKps);
+    
+    const bodyKps = [{time: 0, start: {x: shoulder.x, y: shoulder.y + bodyWidth/8}, end: {x: hip.x, y: hip.y - bodyWidth/8}}];
+
+    const kpDurations = getKpDurations(hipKps);
+
+    React.useEffect(() => {
+        range(hipKps.length).forEach(i => tl.to(bodyRef.current, {y: hipKps[(i + 1) % hipKps.length].y - hip.y, duration: kpDurations[i]}, i === 0 ? 0 : '>'));
+    }, [...hipKps, tl]);
 
     return (
         <g>
-          <WalkinLegCycle hip={hip} legWidth={legWidth} legLength={legLength} bendRadius={bendRadius} color={color} footAmplX={footAmplX} footAmplY={footAmplY} hipAmpl={hipAmpl} freq={freq} phi={0} tl={tl} />
-          <WalkinLegCycle hip={hip} legWidth={legWidth} legLength={legLength} bendRadius={bendRadius} color={color} footAmplX={footAmplX} footAmplY={footAmplY} hipAmpl={hipAmpl} freq={freq} phi={Math.PI} tl={tl} />
+          <AnimatedHoseCycle hoseKps={lLegKps} hoseWidth={legWidth} hoseLength={legLength} bendRadius={legBendRadius} color={color} tl={tl} />
+          <g ref={bodyRef}>
+            <circle fill={color} cx={head.x} cy={head.y} r={headSize/2} />
+            <AnimatedHoseCycle hoseKps={lArmKps} hoseWidth={armWidth} hoseLength={armLength} bendRadius={armBendRadius} color={color} tl={tl} />
+            <AnimatedHoseCycle hoseKps={bodyKps} hoseWidth={bodyWidth} hoseLength={bodyHeight} bendRadius={bodyBendRadius} color={color} tl={tl} />
+            <AnimatedHoseCycle hoseKps={rArmKps} hoseWidth={armWidth} hoseLength={armLength} bendRadius={armBendRadius} color={color} tl={tl} />
+          </g>
+          <AnimatedHoseCycle hoseKps={rLegKps} hoseWidth={legWidth} hoseLength={legLength} bendRadius={legBendRadius} color={color} tl={tl} />
         </g>
     );
 }
 
+const getHoseKeypoints = (startKps, endKps) => {
+    return startKps.map((startKp, i) => {
+        return {
+            time: startKp.time,
+            start: {
+                x: startKp.x,
+                y: startKp.y
+            },
+            end: {
+                x: endKps[i].x,
+                y: endKps[i].y
+            }
+        };
+    });
+};
 
-const WalkinLegCycle = ({hip, legWidth, legLength, bendRadius=2.0, color, footAmplX, footAmplY, hipAmpl, freq, phi, tl}) => {
-    const legRef = React.useRef();
+const getKeypoints = (times, getPointAtTime) => {
+    return times.map(t => {
+        return {
+            time: t,
+            ...getPointAtTime(t)
+        }
+    });
+};
 
-    const numKeypoints = Math.round(24/freq);
-    const period = 1/freq;
-    const kpDuration = period/numKeypoints;
-    const t0 = phi/(2*Math.PI)*period;
-    const kps = range(t0, period + t0, kpDuration);
-
-    const pathDs = kps.map(kp => getWalkingLegString(kp, hip, legLength, footAmplX, footAmplY, hipAmpl, freq, bendRadius));
-    
-    React.useEffect(() => {
-        range(pathDs.length).forEach(i => tl.to(legRef.current, {morphSVG: pathDs[(i + 1) % pathDs.length], duration: kpDuration}, i === 0 ? 0 : '>'));
+const getKpDurations = (kps) => {
+    return kps.reduce((prev, curr, i, arr) => {
+        prev.push(i === 0 ? curr.time : curr.time - arr[i - 1].time);
+        return prev;
     }, []);
-    
+};
+
+const AnimatedHoseCycle = ({hoseWidth, hoseLength, hoseKps, bendRadius, color, tl}) => {
+    const hoseRef = React.useRef();
+
+    const kpDurations = getKpDurations(hoseKps);
+    const pathDs = hoseKps.map(kp => getRubberHoseString(kp.start, kp.end, hoseLength, bendRadius));
+
+    React.useEffect(() => {
+        range(pathDs.length).forEach(i => tl.to(hoseRef.current, {morphSVG: pathDs[(i + 1) % pathDs.length], duration: kpDurations[i]}, i === 0 ? 0 : '>'));
+    }, [...pathDs, tl]);
+
     return (
-        <path ref={legRef} d={pathDs[0]} stroke={color} fillOpacity={0} strokeWidth={legWidth} strokeLinecap="round" />
+        <path ref={hoseRef} d={pathDs[0]} stroke={color} fillOpacity={0} strokeWidth={hoseWidth} strokeLinecap="round" />
     );
 }
 
-const getWalkingLegString = (time, hip, legLength, footAmplX, footAmplY, hipAmpl, freq, bendRadius) => {
-    const foot = {
+const getWalkingHip = (time, hip, ampl, walkFreq, phi) => {
+    return {
         x: hip.x,
-        y: hip.y + legLength * 4/5
+        y: hip.y + ampl.y * Math.sin(4 * Math.PI * walkFreq * time)
     };
-    const newFoot = {
-        x: foot.x + footAmplX * Math.sin(2 * Math.PI * freq * time),
-        y: foot.y - footAmplY * Math.max(0, Math.cos(2 * Math.PI * freq * time))
-    };
-    const newHip = {
-        x: hip.x,
-        y: hip.y + hipAmpl * Math.sin(4 * Math.PI * freq * time)
-    };
-
-
-    return getRubberHoseString(newHip, newFoot, legLength, bendRadius);
 };
 
+const getWalkingFoot = (time, foot, ampl, freq, phi) => {
+    return {
+        x: foot.x + ampl.x * Math.sin(2 * Math.PI * freq * time + phi),
+        y: foot.y - ampl.y * Math.max(0, Math.cos(2 * Math.PI * freq * time + phi))
+    };
+};
+
+const getWalkingShoulder = (time, shoulder, ampl, freq, phi) => {
+    return {
+        x: shoulder.x,
+        y: shoulder.y + ampl.y * Math.cos(2 * Math.PI * freq * time + phi),
+    };
+};
+
+const getWalkingHand = (time, hand, ampl, freq, phi) => {
+    return {
+        x: hand.x + ampl.x * Math.sin(2 * Math.PI * freq * time + phi),
+        y: hand.y - ampl.y + Math.abs(ampl.y * Math.cos(2 * Math.PI * freq * time + phi)),
+    };
+};
 
 const getRubberHoseString = (start, end, hoseLength, bendRadius) => {
     const [ctrlStart, ctrlEnd] = getRubberHoseCtrlPoints(start, end, hoseLength, bendRadius);
     return `M ${start.x},${start.y} C ${ctrlStart.x},${ctrlStart.y} ${ctrlEnd.x},${ctrlEnd.y} ${end.x},${end.y}`;
 };
-
 
 const getRubberHoseCtrlPoints = (start, end, hoseLength, bendRadius) => {
     const eps = 0.0001;
