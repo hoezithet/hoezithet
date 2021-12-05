eac7dacea3d2d74014f1625eacb06a91f1314ff7
Floris De Feyter <floris@hoezithet.nu>
2021-12-05 07:10:56 +0100

    Add WalkingPerson

---
+++ content/fysica/grootheden_eenheden/eenheden_omzetten/drawings/walkingPerson.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { DrawingContext } from "components/shortcodes/drawing";
+import { gsap } from "gsap";
+import {
+    Person, addPoseKeypointToTl, getRestPose
+} from "components/drawings/person";
+import withSizePositionAngle from "components/withSizePositionAngle";
+import { getWalkKeypoints } from "./walkCycleKps";
+
+export const withWalking = (Component) => {
+    return ({repeat = -1, initialPose = null, ...props}) => {
+        const personRef = React.useRef();
+        const { addAnimation } = React.useContext(DrawingContext);
+        const [tl, setTl] = React.useState(() => gsap.timeline({repeat: repeat}));
+
+        React.useEffect(() => {
+            tl.clear();
+            initialPose = initialPose === null ? getRestPose() : initialPose;
+            const poseKps = getWalkKeypoints({initialPose: initialPose});
+            poseKps.forEach(kp => addPoseKeypointToTl(kp, personRef, tl));
+            addAnimation(tl, 0);
+        }, []);
+
+        return (
+            <Component ref={personRef} {...props} />
+        );
+    }
+};
+
+
+const WalkingPerson = withSizePositionAngle(withWalking(Person), 100, 100);
+
+export default WalkingPerson;
