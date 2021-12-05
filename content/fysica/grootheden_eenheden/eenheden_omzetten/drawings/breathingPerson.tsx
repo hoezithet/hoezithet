0e70b0e3a5c6869121614a9613821ff799a87a9f
Floris De Feyter <floris@hoezithet.nu>
2021-12-05 07:12:05 +0100

    Add BreathingPerson

---
+++ content/fysica/grootheden_eenheden/eenheden_omzetten/drawings/breathingPerson.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { DrawingContext } from "components/shortcodes/drawing";
+import { gsap } from "gsap";
+import {
+    Person, addPoseKeypointToTl, getRestPose
+} from "components/drawings/person";
+import withSizePositionAngle from "components/withSizePositionAngle";
+import { getBreathingKeypoints } from "./breatheCycleKps";
+
+export const withBreathing = (Component) => {
+    return ({
+        repeat = -1, initialPose = null, breatheFreq = 0.25, breatheAmpl = 2, ...props
+    }) => {
+        const personRef = React.useRef();
+        const { addAnimation } = React.useContext(DrawingContext);
+        const [tl, setTl] = React.useState(() => gsap.timeline({repeat: repeat}));
+
+        React.useEffect(() => {
+            tl.clear();
+            initialPose = initialPose === null ? getRestPose() : initialPose;
+
+            const restTl = gsap.timeline({repeat: -1, paused: true});
+            const breatheKps = getBreathingKeypoints({
+                initialPose: initialPose,
+                freq: breatheFreq,
+                ampl: breatheAmpl,
+            });
+            breatheKps.forEach(kp => addPoseKeypointToTl(kp, personRef, restTl));
+
+            tl.add(restTl.play());
+            addAnimation(tl, 0);
+        }, []);
+
+        return (
+            <Component ref={personRef} {...props} />
+        );
+    }
+};
+
+
+const BreathingPerson = withSizePositionAngle(withBreathing(Person), 100, 100);
+
+export default BreathingPerson;
