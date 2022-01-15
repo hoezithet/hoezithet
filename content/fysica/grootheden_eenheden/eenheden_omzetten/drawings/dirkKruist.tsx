2fb95de745d188633b835c12a3490812cd6ffad8
Floris De Feyter <floris@hoezithet.nu>
2022-01-15 09:17:40 +0100

    Better text balloon font size

--- content/fysica/grootheden_eenheden/eenheden_omzetten/drawings/dirkKruist.tsx
+++ content/fysica/grootheden_eenheden/eenheden_omzetten/drawings/dirkKruist.tsx
@@ -19,7 +19,8 @@ const WalkingToStopPerson = () => {
     const { addAnimation, xScale } = React.useContext(DrawingContext);
     const [tl, setTl] = React.useState(() => gsap.timeline());
 
-    const freq = 0.75;
+    const stepFreq = 2;
+    const breatheFreq = 0.2;
     const stepSize = 100;
     const startPos = 0;
     const endPos = 1920*1/3;
@@ -35,21 +36,21 @@ const WalkingToStopPerson = () => {
             x: startPosPx,
         }, {
             x: endPosPx,
-            duration: numCycles/freq,
+            duration: numCycles/stepFreq,
             ease: "none",
         });
         addAnimation(tl, 0);
-    }, [startPosPx, endPosPx, numCycles, freq]);
+    }, [startPosPx, endPosPx, numCycles, stepFreq]);
 
     return (
         <g ref={ref}>
-            <_WalkingToStopPerson numCycles={numCycles} stepSize={stepSizePx} freq={freq} height={600} x={-100} y={1080-50} vAlign="bottom" hAlign="right" />
+            <_WalkingToStopPerson numCycles={numCycles} stepSize={stepSizePx} stepFreq={stepFreq} breatheFreq={breatheFreq} height={600} x={-100} y={1080-50} vAlign="bottom" hAlign="right" />
         </g>
     );
 };
 
 
-const TextBallon = ({text, gsapPosition}) => {
+const TextBallon = ({text, gsapPosition, width=1000}) => {
     const [noteX, noteY] = [1509, 350];
     const ref = React.useRef();
     const { addAnimation } = React.useContext(DrawingContext);
@@ -66,7 +67,7 @@ const TextBallon = ({text, gsapPosition}) => {
           <AnnotArrow annot={{x: noteX - 50, y: noteY - 50}} target={{x: 1445, y: 440}}
               margin={0} hAlignTarget="left"
               hAlignAnnot="right" vAlignAnnot="bottom" hideHead color="light_gray"/>
-          <SvgNote x={noteX} y={noteY} width={1000}
+          <SvgNote x={noteX} y={noteY} width={width} fontSize={12}
               hAlign="right"
               vAlign="bottom"
               showBackground>
@@ -86,7 +87,7 @@ const DirkKruist = () => {
             <LoopingWolken speed={-5} width={2*1642.394890} height={346.619760} x={0} y={92.659100} />
             <InfScrollingPark width={3840} x={-1000} />
             <WalkingToStopPerson />
-            <TextBallon text={String.raw`Ik ben $1{,}84~\si{m}$ groot!`} gsapPosition={4}/>
+            <TextBallon text={String.raw`Ik ben $1{,}84~\si{m}$ groot!`} gsapPosition={1}/>
             <BreathingDirk flipH height={dirkHeight} x={dirkX} y={dirkY} vAlign="bottom" hAlign="left"/>
         </Drawing>
     );
@@ -104,7 +105,7 @@ export const DirkZegtTijd = () => {
             <LoopingWolken speed={-5} width={2*1642.394890} height={346.619760} x={0} y={92.659100} />
             <InfScrollingPark width={3840} x={-1000} />
             <BreathingPerson freq={0.5} height={600} x={1920*1/3} y={1080-50} vAlign="bottom" hAlign="right"/>
-            <TextBallon text={String.raw`Ik loop al $5623~\si{s}$ rond in dit park!`} gsapPosition={0}/>
+            <TextBallon text={String.raw`Ik loop al $5623~\si{s}$ rond in dit park!`} gsapPosition={0} width={900}/>
             <BreathingDirk flipH height={dirkHeight} x={dirkX} y={dirkY} vAlign="bottom" hAlign="left"/>
         </Drawing>
     );
