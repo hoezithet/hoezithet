4fde48915c9d791f7d9502fc64d4f745011db180
Floris De Feyter <floris_defeyter@hotmail.com>
2022-05-05 22:29:28 +0200

    Remove ignoreDrawingContext

--- src/components/withInfLoop.tsx
+++ src/components/withInfLoop.tsx
@@ -4,12 +4,12 @@ import { gsap } from "gsap";
 
 
 const withInfLoop = (Component) => {
-    return ({speed=0, x, width, ignoreDrawingContext, ...props}) => {
-        const { width: drawingWidth, addAnimation, xScale } = React.useContext(DrawingContext);
+    return ({speed=0, x, width, ...props}) => {
+        const { addAnimation } = React.useContext(DrawingContext);
 
-        const xOrigin = ignoreDrawingContext ? 0 : xScale(0);
-        const widthPx = ignoreDrawingContext ? width : Math.abs(xScale(width) - xScale(0));
-        const max_shift_px = Math.max(widthPx, drawingWidth);
+        const xOrigin = 0;
+        const widthPx = width;
+        const max_shift_px = widthPx;
         const ref1 = React.useRef();
         const ref2 = React.useRef();
         const ref3 = React.useRef();
@@ -20,7 +20,7 @@ const withInfLoop = (Component) => {
             }
             const tl = gsap.timeline({repeat: -1});
             tl.fromTo([ref1, ref2, ref3].map(ref => ref.current), {
-                x: ignoreDrawingContext ? x : xScale(x),
+                x: x,
             }, {
                 x: `+=${Math.sign(speed)*max_shift_px}`,
                 duration: max_shift_px/Math.abs(speed),
@@ -29,7 +29,7 @@ const withInfLoop = (Component) => {
             addAnimation(tl, 0);
         }, [x, speed, max_shift_px]);
 
-        const shift = ignoreDrawingContext ? max_shift_px : xScale.invert(max_shift_px + xOrigin);
+        const shift = max_shift_px;
 
         return (
             <>
