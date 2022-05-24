b0969d150a4f2828e2ba4f74fbd2856e17493b91
Floris De Feyter <floris_defeyter@hotmail.com>
2022-05-24 11:21:02 +0200

    Make callout expandable

--- src/components/callout.tsx
+++ src/components/callout.tsx
@@ -2,6 +2,10 @@ import React from "react";
 
 import styled from "styled-components";
 import COLORS, {getColor} from "colors";
+import _ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { gsap } from 'gsap';
+import BareLessonContext from "contexts/bareLessonContext";
+import _ from "lodash";
 
 
 const Frame = styled.div`
@@ -21,6 +25,7 @@ const TitleBox = styled.div`
     color: ${props => getColor(props.color)};
     font-size: ${TITLE_FONT_SIZE};
     font-weight: bold;
+    cursor: ${props => props.useExpand ? "pointer" : "auto"};
     & > p {
         margin: 0;
     }
@@ -28,6 +33,11 @@ const TitleBox = styled.div`
 
 const TitleIcon = styled.span`
     float: left;
+`;
+
+const ExpandMoreIcon = styled(_ExpandMoreIcon)`
+    float: right;
+    cursor: pointer;
 `;
 
 const TitleText = styled.span`
@@ -42,6 +52,7 @@ const BodyText = styled.div`
     padding-left: ${props => props.hasIcon ? TEXT_ICON_PAD : "0"};
     margin-top: calc(${props => !props.hasTitle && props.hasIcon ? TITLE_FONT_SIZE : 0}*0.3);
     padding-top: ${props => props.hasTitle ? "1em" : 0};
+    overflow: hidden;
     & > p {
         margin: 0;
     }
@@ -57,8 +68,11 @@ const ICON_COLORS = {
     WARN: "red",
 };
 
-const splitIconTitleBody = (props) => {
-    let [title, body, icon] = [null, null, null];
+const EXPANDED = '+';
+const COLLAPSED = '-';
+
+const splitIconTitleBodyExpanded = (props) => {
+    let [title, body, icon, expanded] = [null, null, null, null];
 
     if (Array.isArray(props.children)) {
         title = props.children[0];
@@ -99,24 +113,125 @@ const splitIconTitleBody = (props) => {
         title = null;
     }
 
-    return [icon, title, body];
+    if (title?.props?.children?.length > 0 && [EXPANDED, COLLAPSED].includes(title.props.children[0])) {
+        expanded = title.props.children[0] === EXPANDED;
+        title = title.props.children.slice(1);
+    } else if (title?.props?.children?.length > 0 && [EXPANDED, COLLAPSED].includes(title.props.children[0][0])) {
+        expanded = title.props.children[0][0] === EXPANDED;
+        title = [
+           ...title.props.children[0].slice(1),
+           ...title.props.children.slice(1),
+       ];
+    } else if (title?.length > 0 && [EXPANDED, COLLAPSED].includes(title[0])) {
+        expanded = title[0] === EXPANDED;
+        title = title.slice(1);
+    }
+    return [icon, title, body, expanded];
 };
 
 
+const useBareCallout = (title, body) => {
+    const bareContext = React.useContext(BareLessonContext);
+    const insideBare = bareContext !== null;
+    const [appendixIdx, setAppendixIdx] = React.useState(-1);
+    const [expId] = React.useState(() => _.uniqueId("expand_"));
+    const [appendixId] = React.useState(() => _.uniqueId("appendix_"));
+
+    const idxToHtmlId = idx => `appendix_${idx}`;
+
+    React.useEffect(() => {
+        if (insideBare) {
+            const {setAppendixItems} = bareContext;
+            setAppendixItems((prevItems) => {
+                const idx = prevItems.length + 1;
+                setAppendixIdx(idx);
+                return [
+                   ...prevItems,
+                   {
+                       appendixId: appendixId,
+                       idx: idx,
+                       title: title,
+                       children: body,
+                       expandId: expId,
+                   }
+                ];
+            });
+        }
+    }, []);
+
+    return [insideBare, expId, appendixId, appendixIdx];
+};
+
+const useExpandableBody = (expanded) => {
+    const [isExpanded, setIsExpanded] = React.useState(expanded);
+    const useExpand = isExpanded !== null;
+    const nodeHeight = React.useRef(0);
+    const nodePaddingTop = React.useRef(0);
+    const nodeRef = React.useRef(null);
+    const bodyRef = React.useCallback(node => {
+        if (node !== null) {
+            nodeRef.current = node;
+            nodeHeight.current = node.clientHeight;
+            nodePaddingTop.current = gsap.getProperty(node, "paddingTop");
+            if (useExpand && !isExpanded) {
+                gsap.set(node, {
+                    height: 0,
+                    paddingTop: 0,
+                });
+            }
+        }
+    }, []);
+    const iconRef = React.useRef(null);
+    const [tl] = React.useState(() => gsap.timeline());
+
+    const handleClick = () => {
+        if (!useExpand) {
+            return;
+        }
+        setIsExpanded(prev => !prev);
+    };
+
+    React.useEffect(() => {
+        if (!useExpand) {
+            return;
+        }
+
+        tl.to(nodeRef.current, {
+            height: isExpanded ? `${nodeHeight.current}px` : 0,
+            paddingTop: isExpanded ? `${nodePaddingTop.current}px` : 0,
+        }).to(iconRef.current, {
+            rotation: isExpanded ? 0 : -90,
+            duration: 0.1,
+        }, "<");
+    }, [isExpanded]);
+    
+    return [useExpand, bodyRef, iconRef, handleClick];
+};
 
 const Callout = (props) => {
-    const [icon, title, body] = splitIconTitleBody(props);
+    const [icon, title, body, expanded] = splitIconTitleBodyExpanded(props);
     const iconType = ICONS[icon];
     const color = ICON_COLORS[iconType] || "gray";
+    const [useExpand, bodyRef, iconRef, handleClick] = useExpandableBody(expanded);
+    const [insideBare, expId, appendixId, appendixIdx] = useBareCallout(title, body);
+
     return (
+        insideBare && useExpand ?
+        <Frame id={expId} color={color}>
+	        Zie <a href={`#${appendixId}`}>appendix {appendixIdx}: "{title}"</a>
+	    </Frame>
+	    :
         <Frame color={color}>
             { (title !== null || icon !== null)
-              ? <TitleBox color={color}>
+              ? <TitleBox color={color} useExpand={useExpand} onClick={useExpand ? handleClick : () => {}}>
                     <TitleIcon>{ icon }</TitleIcon>
-                    <TitleText hasIcon={icon !== null}>{ title }</TitleText>
+                    <TitleText hasIcon={icon !== null}>
+                        { title }
+                        {useExpand ? <ExpandMoreIcon ref={iconRef} /> : null}
+                    </TitleText>
                 </TitleBox>
               : null }
-            <BodyText hasIcon={icon !== null} hasTitle={title !== null}>{ body }</BodyText>
+            <BodyText ref={bodyRef} hasIcon={icon !== null} hasTitle={title !== null}>{ body }</BodyText>
         </Frame>
     );
 };
