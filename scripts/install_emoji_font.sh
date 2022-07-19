8b2723ab42ac21799d84004569e6e731bf231b4a
Floris De Feyter <floris_defeyter@hotmail.com>
2022-07-19 12:49:57 +0200

    Install emoji font for Puppeteer PDFs

---
+++ scripts/install_emoji_font.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+srcdir="/tmp/Quicksand"
+pkgdir="/usr/share/fonts/truetype/NotoColorEmoji"
+
+mkdir $srcdir
+cd $srcdir
+echo "Downloading NotoColorEmoji..."
+wget https://github.com/googlefonts/noto-emoji/raw/main/fonts/NotoColorEmoji.ttf
+
+echo "Installing NotoColorEmoji..."
+sudo mkdir -p $pkgdir
+sudo find $srcdir -type f -name "*.ttf" -exec install -Dm644 {} $pkgdir \;
+
+echo "Updating font-cache..."
+sudo fc-cache -f > /dev/null
+
+echo "Done!"
