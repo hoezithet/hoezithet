40c03956a65f976fcd9fab67cea1d6476cd15f6c
Floris De Feyter <floris_defeyter@hotmail.com>
2022-08-01 10:18:27 +0200

    Fix: create new DrawingContext with correct width and height

--- src/components/withDrawingScale.tsx
+++ src/components/withDrawingScale.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { DrawingContext } from "components/drawings/drawing";
+import { DrawingContext, scaleLinear } from "components/drawings/drawing";
 
 
 const withDrawingScale = <P> (
@@ -8,14 +8,25 @@ const withDrawingScale = <P> (
     normHeight: number,
 ): React.FC<P> => {
     return (props: P) => {
-        const { xScale, yScale } = React.useContext(DrawingContext);
+        const { xScale, yScale, ref } = React.useContext(DrawingContext);
 
         const scaleX = xScale.metric(normWidth)/normWidth;
         const scaleY = yScale.metric(normHeight)/normHeight;
 
+        const newXScale = scaleLinear({
+            range: [0, normWidth],
+            domain: [0, normWidth],
+        });
+        const newYScale = scaleLinear({
+            range: [0, normHeight],
+            domain: [0, normHeight],
+        });
+
         return (
             <g transform={`scale(${scaleX} ${scaleY})`}>
-                <Component {...props} />
+                <DrawingContext.Provider value={{width: normWidth, height: normHeight, xScale: newXScale, yScale: newYScale, ref: ref}}>
+                    <Component {...props} />
+                </DrawingContext.Provider>
             </g>
         );
     };
