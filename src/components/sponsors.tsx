71fb510b8dca62aceaf685f4df7d4a5c01572881
Floris De Feyter <floris_defeyter@hotmail.com>
2022-09-12 16:38:30 +0200

    Remove KU Leuven sponsor

--- src/components/sponsors.tsx
+++ src/components/sponsors.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import kuLeuvenLogo from "../../images/sponsor/KULEUVEN_RGB_LOGO.png";
 import vivesLogo from "../../images/sponsor/VIVES.png";
 import { Link } from 'gatsby-theme-material-ui';
 import Grid from "@mui/material/Grid";
@@ -11,11 +10,6 @@ export default function Sponsors({ width = "56mm", showTreat = true}) {
         <div style={{breakInside: "avoid"}}>
         <p style={{fontWeight: "bold"}}>Hoe Zit Het? wordt met trots gesteund door</p>
         <Grid container spacing={ 4 } justifyContent="center" alignItems="center" >
-          <Grid item>
-            <a href="https://www.kuleuven.be/kuleuven">
-                <img src={ kuLeuvenLogo } style={imgStyles} alt="KU Leuven sponsor" />
-            </a>
-          </Grid>
           <Grid item>
             <a href="https://www.vives.be/">
                 <img src={ vivesLogo } style={imgStyles} alt="VIVES sponsor" />
