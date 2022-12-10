b8b9b2bf98058938b6382c0a3067bb22237cfb05
Floris De Feyter <floris_defeyter@hotmail.com>
2022-12-10 15:16:44 +0100

    Add FillAny

--- src/components/exercises/fillAnswer.tsx
+++ src/components/exercises/fillAnswer.tsx
@@ -12,21 +12,34 @@ type FillStringProps = {
 };
 
 const _FillString = ({ children, solution }: FillStringAnswerProps) => {
+    const evaluateAnswerValue = (v: string|null) => v !== null && String(v) === String(solution);
+    return <_FillAny evaluate={evaluateAnswerValue}>{ children }</_FillAny>
+};
+
+
+type FillAnyProps = {
+    children: React.ReactNode,
+    evaluate: (answerValue: any) => boolean,
+    solution: string,
+    placeholder?: string,
+    variant?: string,
+};
+
+const _FillAny = ({ children, evaluate, solution, placeholder="Vul in", variant="filled" }: FillAnyProps) => {
     const explanation = getChildAtIndex(children, 0) || null;
-    const evaluateAnswerValue = (v: string|null) => v === String(solution);
-    const {answerValue, setAnswerValue, showingSolution} = useAnswerValue(evaluateAnswerValue, solution, explanation);
+    const {answerValue, setAnswerValue, showingSolution} = useAnswerValue(evaluate, solution, explanation);
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (e.target.value === "") {
             setAnswerValue(null);
         } else {
-            setAnswerValue(String(e.target.value));
+            setAnswerValue(e.target.value);
         }
     }
 
     return (
-        <TextField disabled={showingSolution} variant="filled" onChange={handleChange}
-            placeholder={showingSolution && answerValue !== null ? answerValue : "Vul in"} value={answerValue !== null ? answerValue : "" } />
+        <TextField disabled={showingSolution} variant={variant} onChange={handleChange}
+            placeholder={showingSolution ? answerValue : placeholder} value={answerValue} />
     );
 };
 
@@ -37,3 +50,12 @@ const _FillString = ({ children, solution }: FillStringAnswerProps) => {
  * @prop {string} solution The solution. Only when a given answer is equal to the solution by string equality, the answer will be evaluated as "correct".
  */
 export const FillString = withFeedback(_FillString);
+
+
+/**
+ * An answer where the user needs to respond in a text field and the evaluation can be done with strict string comparison.
+ *
+ * @prop {React.ReactNode} children If present, the first child node will be used as the explanation of the solution. All other children will be ignored.
+ * @prop {string} solution The solution. Only when a given answer is equal to the solution by string equality, the answer will be evaluated as "correct".
+ */
+ export const FillAny = withFeedback(_FillAny);
