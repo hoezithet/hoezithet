29977a9e2d3c26b48fe89ac514916de75992a507
Floris De Feyter <floris_defeyter@hotmail.com>
2025-01-07 17:17:19 +0100

    Use npm run iso gatsby command

--- deploy.sh
+++ deploy.sh
@@ -14,8 +14,8 @@ npm config set //npm.greensock.com/:_authToken=$GSAP_TOKEN
 npm config set @gsap:registry=https://npm.greensock.com
 npm install gsap@npm:@gsap/shockingly --legacy-peer-deps
 npm install --legacy-peer-deps
-gatsby clean
-gatsby build &&\
+npm run clean
+npm run build &&\
 rsync -r $WORKSPACE/public/ &&\
 node node_modules/puppeteer/install.js &&\
 node scripts/create_lesson_pdfs_pngs.js --host=$(if [[ $BRANCH_NAME = "develop" ]]; then echo "dev.hoezithet.nu"; else echo "hoezithet.nu"; fi) --cwd=$WORKSPACE &&\
