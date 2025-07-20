0b19c555587d234b08ed9f300ec11cc7c517ed3a
Floris De Feyter <floris_defeyter@hotmail.com>
2025-07-20 08:55:53 +0200

    Update deploy.sh for Cloudflare hosting

--- deploy.sh
+++ deploy.sh
@@ -2,21 +2,15 @@
 WORKSPACE=$(pwd)
 BRANCH_NAME=$(git symbolic-ref --short -q HEAD)
 export $(cat .env.production | xargs)
-HOST=$HZH_USER@hoezithet.nu
-DEST=$HZH_DIR/public_$BRANCH_NAME
-BCKP=$DEST.bckp
-echo $DEST
-echo "Removing old backup $BCKP..."
-ssh $HOST rm -r $BCKP
-echo "Backing up $DEST to $BCKP..."
-ssh $HOST cp -r $DEST $BCKP
-npm config set //npm.greensock.com/:_authToken=$GSAP_TOKEN
-npm config set @gsap:registry=https://npm.greensock.com
-npm install gsap@npm:@gsap/shockingly --legacy-peer-deps
+source ~/.nvm/nvm.sh
+nvm use 16.20.2 &&\
 npm install --legacy-peer-deps
 npm run clean
 npm run build &&\
-rsync -r $WORKSPACE/public/ &&\
+nvm use 20.16.0 &&\
+npx wrangler pages deploy $WORKSPACE/public --project-name=hoezithet &&\
+nvm use 16.20.2 &&\
 node node_modules/puppeteer/install.js &&\
 node scripts/create_lesson_pdfs_pngs.js --host=$(if [[ $BRANCH_NAME = "develop" ]]; then echo "dev.hoezithet.nu"; else echo "hoezithet.nu"; fi) --cwd=$WORKSPACE &&\
-rsync -r $WORKSPACE/public/ $HZH_USER@hoezithet.nu:$HZH_DIR/public_$BRANCH_NAME
+nvm use 20.16.0 &&\
+npx wrangler pages deploy $WORKSPACE/public --project-name=hoezithet
