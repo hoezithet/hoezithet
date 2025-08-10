63e7086be6c74fd21c9d7195a3a844e833d60f1f
Floris De Feyter <floris_defeyter@hotmail.com>
2025-08-10 18:08:46 +0200

    Add example table housing prices

--- content/ai/ann/intro/drawings.tsx
+++ content/ai/ann/intro/drawings.tsx
@@ -494,12 +494,12 @@ const SumActNeuron = ({x, y, sumOutput, radius, color, activFunc}) => {
     );
 };
 
-export const EriksNeuron2 = ({}) => {
+export const EriksNeuron2 = ({opp=70, afst=100}) => {
     return (
         <InteractiveMLP
             inputProps={[
-                {value: 70, min: 0, max: 500, step: 10, name: "\\text{Opp}", unit: "\\si{m}^2"},
-                {value: 100, min: 0, max: 200, step: 1, name: "\\text{Afst}", unit: "\\si{km}"},
+                {value: opp, min: 0, max: 500, step: 1, name: "\\text{Opp}", unit: "\\si{m}^2"},
+                {value: afst, min: 0, max: 200, step: 0.1, name: "\\text{Afst}", unit: "\\si{km}"},
             ]}
             outputProps={[
                 {name: "\\text{Waarde}", unit: "\\si{euro}"},
